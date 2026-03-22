2c977271c00b7fc48bb47206680b8532c24f5b44
LUKSOAgent <motors.scrims_3d@icloud.com>
2026-03-22 15:03:52 +0000

    fix: update trust-graph API to correct proxy address (11 agents)

--- frontend/api/trust-graph.js
+++ frontend/api/trust-graph.js
@@ -18,7 +18,7 @@
 import { ethers } from "ethers";
 
 const RPC = "https://rpc.mainnet.lukso.network";
-const CONTRACT = "0x1581BA9Fb480b72df3e54f51f851a644483c6ec7";
+const CONTRACT = "0x16505FeC789F4553Ea88d812711A0E913D926ADD";
 const ENVIO_ENDPOINT = "https://envio.lukso-mainnet.universal.tech/v1/graphql";
 
 const ABI = [
